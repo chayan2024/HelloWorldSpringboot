c8cf57cf7fcf7e73d53e4d989d8f3dcd730edb1c
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-05-03 00:32:54 +0530

    Initial commit

---
+++ src/main/java/com/example/HelloWorldSpringboot/HelloWorldSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringbootApplication.class, args);
+	}
+
+}
